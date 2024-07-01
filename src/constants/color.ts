638f3af622d2d0f0e67f32a3c6dde856512d25b8
yeonddori <126975394+yeonddori@users.noreply.github.com>
2024-07-02 00:15:51 +0900

    커스텀 색상 추가 및 폰트 지정 (#6)
    * design: 커스텀 색상 추가
    
    - primary, secondary 색상 지정
    - typography font 지정
    
    #5
    
    * fix: 폴더명 workflow -> workflows

---
+++ src/constants/color.ts
@@ -0,0 +1,7 @@
+const color = {
+  brown_light: '#D8D1C5',
+  gray_light: '#BDBDBD',
+  gray_dark: '#666666',
+};
+
+export default color;
---
+++ src/constants/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material';
+import color from './color';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: color.brown_light,
+      contrastText: 'white',
+    },
+    secondary: {
+      main: color.gray_dark,
+    },
+  },
+
+  typography: {
+    fontFamily: 'Noto Sans, sans-serif',
+  },
+});
+
+export default theme;
