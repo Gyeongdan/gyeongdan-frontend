459bf50c84e743ff0b22f976a9a7604de3892df4
yeonddori <lsymiso@naver.com>
2024-07-04 16:47:49 +0900

    design: 폰트 사이즈 지정
    #14

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -15,6 +15,27 @@ const theme = createTheme({
 
   typography: {
     fontFamily: 'Noto Sans, sans-serif',
+    h1: {
+      fontSize: '36px',
+      fontWeight: 'bold',
+    },
+    h2: {
+      fontSize: '24px',
+      fontWeight: 'bold',
+    },
+    h3: {
+      fontSize: '20px',
+      fontWeight: 'bold',
+    },
+    subtitle1: {
+      fontSize: '20px',
+    },
+    body1: {
+      fontSize: '16px',
+    },
+    body2: {
+      fontSize: '14px',
+    },
   },
 });
 
