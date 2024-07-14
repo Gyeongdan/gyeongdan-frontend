a548009a5b5284fc62c20fd5eaf8d9879ee75bd1
yeonddori <lsymiso@naver.com>
2024-07-14 14:33:47 +0900

    refactor: 이미지 로딩 형식 변경
    #26

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['wimg.mk.co.kr', 'img.hankyung.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wimg.mk.co.kr',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.hankyung.com',
+      },
+    ],
   },
 };
 
