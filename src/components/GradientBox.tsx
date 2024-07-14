f837f3eef09cb78db225e86043b8f5e9d06d691c
yeonddori <lsymiso@naver.com>
2024-07-14 16:07:57 +0900

    feat: 그라데이션 배경 box 구현
    #29

---
+++ src/components/GradientBox.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { Box, BoxProps, styled } from '@mui/material';
+
+interface GradientBoxProps extends BoxProps {
+  children?: React.ReactNode;
+}
+
+const StyledGradientBox = styled(Box)({
+  background: 'linear-gradient(rgba(29, 112, 241, 0), rgba(26, 197, 251, 1))',
+  opacity: 0.2,
+  height: '100%',
+  width: '100%',
+});
+
+const GradientBox = ({ children, ...props }: GradientBoxProps) => {
+  return (
+    <StyledGradientBox className={props.className} sx={props.sx}>
+      {children}
+    </StyledGradientBox>
+  );
+};
+
+GradientBox.defaultProps = {
+  children: null,
+};
+
+export default GradientBox;
