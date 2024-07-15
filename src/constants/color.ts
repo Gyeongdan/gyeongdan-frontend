88e8b8100b5800a4d8fd57b242b4de29967b6059
stopmin <geemin0@naver.com>
2024-07-16 00:36:15 +0900

    feat: 색상 두가지 추가 - 그라데이션 다크 블루 - 그라데이션 라이트 블루

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -2,6 +2,9 @@ const color = {
   blue: '#4187FF',
   gray_light: '#F8F8F8',
   gray_dark: '#3E3E3E',
+  gradient_blue_dark: '#1D71F1',  // 진한 파란색
+  gradient_blue_light: '#1AC6FB'  // 밝은 파란색
 };
+
 
 export default color;
