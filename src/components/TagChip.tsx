dd89aadc85b32f9cf7ea10e5cdb450a5dc0671f5
stopmin <geemin0@naver.com>
2024-07-16 02:04:05 +0900

    refactor: lint 적용

--- src/components/TagChip.tsx
+++ src/components/TagChip.tsx
@@ -1,11 +1,12 @@
 import Chip from '@mui/material/Chip';
+
 import color from '@/constants/color';
 
 const TagChip = ({ label }: { label: string }) => {
   return (
     <Chip
-      label={label}
       color="primary"
+      label={label}
       sx={{
         background: `linear-gradient(${color.gradient_blue_dark}, ${color.gradient_blue_light})`,
         display: 'flex',
