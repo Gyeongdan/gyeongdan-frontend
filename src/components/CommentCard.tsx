525ae6346c2c3bcb09c675c7a979a12ab4685522
stopmin <geemin0@naver.com>
2024-07-16 03:26:58 +0900

    feat: isCharacter로 산지니

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'; // next/image를 사용하여 이미지 최적화
 import React from 'react';
 
 import { Card, Typography, Box, SxProps, Theme } from '@mui/material';
@@ -9,12 +10,24 @@ interface CommentCardProps {
   isStroke?: boolean;
   isFilled?: boolean;
   width?: string | number;
+  isCharacter?: boolean;
 }
 
-const CommentCard = ({ content, isStroke = false, isFilled = true, width = '100%' }: CommentCardProps) => {
+const CommentCard = ({
+  content,
+  isStroke = false,
+  isFilled = true,
+  width = '100%',
+  isCharacter = false,
+}: CommentCardProps) => {
+  const containerStyles: SxProps<Theme> = {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '0.5rem 0',
+  };
+
   const boxStyles: SxProps<Theme> = {
     position: 'relative',
-    margin: '0.5rem 0',
     borderRadius: '1.5rem',
     padding: isStroke ? '0.125rem' : '0',
     background: isStroke ? `linear-gradient(${color.gradient_blue_dark}, ${color.gradient_blue_light})` : 'none',
@@ -33,12 +46,23 @@ const CommentCard = ({ content, isStroke = false, isFilled = true, width = '100%
   };
 
   return (
-    <Box sx={boxStyles}>
-      <Card elevation={0} sx={cardStyles}>
-        <Typography color={isFilled ? 'white' : 'text.primary'} sx={textStyles} variant="body2">
-          {content}
-        </Typography>
-      </Card>
+    <Box sx={containerStyles}>
+      {isCharacter && (
+        <Image
+          alt="Character Image"
+          height={100}
+          src="/image/sanjinee.png"
+          style={{ borderRadius: '50%', marginRight: '0.5rem' }}
+          width={100}
+        />
+      )}
+      <Box sx={boxStyles}>
+        <Card elevation={0} sx={cardStyles}>
+          <Typography color={isFilled ? 'white' : 'text.primary'} sx={textStyles} variant="body2">
+            {content}
+          </Typography>
+        </Card>
+      </Box>
     </Box>
   );
 };
