1c3ad9b78cad20e8a002b0537fa6ad9fa291a2ba
stopmin <geemin0@naver.com>
2024-07-16 03:12:32 +0900

    refactor: 주석 제거

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -3,8 +3,8 @@ const color = {
   divider: '#ACCCFF',
   gray_light: '#F8F8F8',
   gray_dark: '#3E3E3E',
-  gradient_blue_dark: '#1D71F1', // 진한 파란색
-  gradient_blue_light: '#1AC6FB', // 밝은 파란색
+  gradient_blue_dark: '#1D71F1',
+  gradient_blue_light: '#1AC6FB',
 };
 
 export default color;
