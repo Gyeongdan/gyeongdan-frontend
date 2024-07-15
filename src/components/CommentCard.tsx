24c67db74b92ad00c64fd07c07b58d7775b4a559
stopmin <geemin0@naver.com>
2024-07-16 05:23:22 +0900

    refactor: 서연 코드리뷰 반영

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image'; // next/image를 사용하여 이미지 최적화
-import React from 'react';
+import Image from 'next/image';
 
-import { Card, Typography, Box, SxProps, Theme } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 import color from '@/constants/color';
 
@@ -9,61 +8,41 @@ interface CommentCardProps {
   content: string;
   isStroke?: boolean;
   isFilled?: boolean;
-  width?: string | number;
   isCharacter?: boolean;
 }
 
-const CommentCard = ({
-  content,
-  isStroke = false,
-  isFilled = true,
-  width = '100%',
-  isCharacter = false,
-}: CommentCardProps) => {
-  const containerStyles: SxProps<Theme> = {
-    display: 'flex',
-    alignItems: 'center',
-    margin: '0.5rem 0',
-  };
-
-  const boxStyles: SxProps<Theme> = {
-    position: 'relative',
-    borderRadius: '1.5rem',
-    padding: isStroke ? '0.125rem' : '0',
-    background: isStroke ? `linear-gradient(${color.gradient_blue_dark}, ${color.gradient_blue_light})` : 'none',
-    width: typeof width === 'number' ? `${width}px` : width,
-  };
-
-  const cardStyles: SxProps<Theme> = {
-    background: isFilled ? `linear-gradient(${color.gradient_blue_dark}, ${color.gradient_blue_light})` : 'white',
-    borderRadius: '1.375rem',
-    padding: '1rem',
-    color: isFilled ? 'white' : 'black',
-  };
-
-  const textStyles: SxProps<Theme> = {
-    whiteSpace: 'pre-line',
-  };
-
+const CommentCard = ({ content, isStroke = false, isFilled = false, isCharacter = false }: CommentCardProps) => {
   return (
-    <Box sx={containerStyles}>
+    <Stack alignItems="center" direction="row">
       {isCharacter && (
-        <Image
-          alt="Character Image"
-          height={100}
-          src="/image/sanjinee.png"
-          style={{ borderRadius: '50%', marginRight: '0.5rem' }}
-          width={100}
-        />
+        <Box height={100} position="relative" width={100}>
+          <Image fill alt="Character Image" sizes="100%" src="/image/sanjinee.png" style={{ objectFit: 'cover' }} />
+        </Box>
       )}
-      <Box sx={boxStyles}>
-        <Card elevation={0} sx={cardStyles}>
-          <Typography color={isFilled ? 'white' : 'text.primary'} sx={textStyles} variant="body2">
+      <Box
+        borderRadius={5}
+        ml={1}
+        padding="2px"
+        sx={{
+          background: isStroke ? `linear-gradient(${color.gradient_blue_dark}, ${color.gradient_blue_light})` : 'none',
+        }}
+        width="100%"
+      >
+        <Box
+          borderRadius="1.1rem"
+          padding={2}
+          sx={{
+            background: isFilled
+              ? `linear-gradient(${color.gradient_blue_dark}, ${color.gradient_blue_light})`
+              : 'white',
+          }}
+        >
+          <Typography color={isFilled ? 'white' : 'black'} variant="body2" whiteSpace="pre-line">
             {content}
           </Typography>
-        </Card>
+        </Box>
       </Box>
-    </Box>
+    </Stack>
   );
 };
 
