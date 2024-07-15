f19a2594285bdbf2ba0d4a4fc40338395c5cc170
정지민 <108014449+stopmin@users.noreply.github.com>
2024-07-16 03:14:25 +0900

    기사의 제목(헤드라인)을 표시하는 부분 생성 (#10)
    * TagChip
    
    * Headline 생성
    
    * Headline 생성
    
    Headline 생성
    
    * 서연 리뷰 반영
    
    * 서연 리뷰 반영
    
    서연 리뷰 반영
    
    * color = "primary"로 수정
    
    * feat: 색상 두가지 추가
    - 그라데이션 다크 블루
    - 그라데이션 라이트 블루
    
    * refactor: 함수 선언방식 수정
    
    * feat: Chip 색상 수정
    
    * refactor: lint 적용
    
    * refactor: lint 적용
    
    * refactor: 리뷰 반영
    
    * refactor: flex 제거
    
    * refactor: lint 적용
    
    * refactor: 주석 제거

---
+++ src/components/TagChip.tsx
@@ -0,0 +1,17 @@
+import Chip from '@mui/material/Chip';
+
+import color from '@/constants/color';
+
+const TagChip = ({ label }: { label: string }) => {
+  return (
+    <Chip
+      color="primary"
+      label={label}
+      sx={{
+        background: `linear-gradient(${color.gradient_blue_dark}, ${color.gradient_blue_light})`,
+      }}
+    />
+  );
+};
+
+export default TagChip;
