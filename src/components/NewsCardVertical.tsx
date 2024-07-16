c4b8ab3b584de214d204276f79e8daf25a06b2b6
yeonddori <126975394+yeonddori@users.noreply.github.com>
2024-07-17 06:33:36 +0900

    feat: 세로형 뉴스 카드 구현 (#46)
    #13

---
+++ src/components/NewsCardVertical.tsx
@@ -0,0 +1,48 @@
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+
+import color from '@/constants/color';
+
+interface NewsCardVerticalProps {
+  date: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+const NewsCardVertical = ({ date, title, description, imageUrl }: NewsCardVerticalProps) => {
+  return (
+    <Card sx={{ maxWidth: 350, boxShadow: 'none' }}>
+      <CardMedia alt="articleImage" component="img" image={imageUrl} sx={{ height: 190, borderRadius: 2 }} />
+      <CardContent>
+        <Typography color={color.blue} fontWeight="600" mb={1} variant="body2">
+          {date}
+        </Typography>
+        <Typography
+          mb={1}
+          sx={{
+            overflow: 'hidden',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+            display: '-webkit-box',
+          }}
+          variant="h5"
+        >
+          {title}
+        </Typography>
+        <Typography
+          sx={{
+            overflow: 'hidden',
+            WebkitLineClamp: 3,
+            WebkitBoxOrient: 'vertical',
+            display: '-webkit-box',
+          }}
+          variant="body2"
+        >
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default NewsCardVertical;
