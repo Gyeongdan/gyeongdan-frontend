f807f23d63d3bf3adf95b259af76515210e5c757
stopmin <geemin0@naver.com>
2024-07-16 15:03:39 +0900

    feat: 배경 색상 살짝 채도 낮춤
    #38

--- src/app/newsletter/[id]/page.tsx
+++ src/app/newsletter/[id]/page.tsx
@@ -1,20 +1,20 @@
 'use client';
 
 import Image from 'next/image';
+
 import { Box, Card, CardContent, Container, Typography } from '@mui/material';
+
+import CommentCard from '@/components/CommentCard';
 import Headline from '@/components/Headline';
 import color from '@/constants/color';
 import articleData from '@/mocks/article';
-import CommentCard from '@/components/CommentCard';
-import article from '@/mocks/article';
-import { padding } from '@mui/system';
 
 const Page = () => {
   return (
     <Box
       color={color.gray_dark}
       sx={{
-        background: `linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(${parseInt(color.gradient_blue_dark.slice(1, 3), 16)}, ${parseInt(color.gradient_blue_dark.slice(3, 5), 16)}, ${parseInt(color.gradient_blue_dark.slice(5, 7), 16)}, 0.2) 50%, rgba(${parseInt(color.gradient_blue_light.slice(1, 3), 16)}, ${parseInt(color.gradient_blue_light.slice(3, 5), 16)}, ${parseInt(color.gradient_blue_light.slice(5, 7), 16)}, 0.2) 100%)`,
+        background: `linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(${parseInt(color.gradient_blue_dark.slice(1, 3), 16)}, ${parseInt(color.gradient_blue_dark.slice(3, 5), 16)}, ${parseInt(color.gradient_blue_dark.slice(5, 7), 16)}, 0.1) 25%, rgba(${parseInt(color.gradient_blue_light.slice(1, 3), 16)}, ${parseInt(color.gradient_blue_light.slice(3, 5), 16)}, ${parseInt(color.gradient_blue_light.slice(5, 7), 16)}, 0.1) 100%)`,
         minHeight: '100vh',
       }}
     >
@@ -28,25 +28,31 @@ const Page = () => {
               viewCount={articleData.viewCount}
             />
           </Box>
-          <CommentCard content={articleData.comment} isCharacter={true} />
+          <CommentCard isCharacter content={articleData.comment} />
           <Box sx={{ marginTop: '1rem', width: '100%' }}>
             <Box sx={{ width: '100%', maxWidth: '600px', margin: '0 auto' }}>
               <Image
                 alt="hi"
-                src={articleData.imageUrl}
-                width={600}
                 height={400}
+                src={articleData.imageUrl}
                 style={{ borderRadius: '8px', width: '100%', objectFit: 'cover' }}
+                width={600}
               />
             </Box>
             <Card sx={{ marginTop: '2rem', backgroundColor: 'transparent', boxShadow: 'none', width: '100%' }}>
               <CardContent sx={{ padding: 0 }}>
                 <Typography
-                  variant="body1"
                   component="div"
-                  sx={{ color: color.gray_dark, whiteSpace: 'pre-line', width: '100%', maxWidth: '600px', margin: '0 auto' }}
+                  sx={{
+                    color: color.gray_dark,
+                    whiteSpace: 'pre-line',
+                    width: '100%',
+                    maxWidth: '600px',
+                    margin: '0 auto',
+                  }}
+                  variant="body1"
                 >
-                  {article.content}
+                  {articleData.content}
                 </Typography>
               </CardContent>
             </Card>
