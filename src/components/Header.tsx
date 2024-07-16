d9a5484e5c76b5727d5038e31b169c09f8d117e5
stopmin <geemin0@naver.com>
2024-07-17 04:30:13 +0900

    refactor: React 의존성 제거
    #40

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Button, Link, Stack, Typography, Box } from '@mui/material';
 
