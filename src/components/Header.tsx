22d4bedd501f7ac8a7136047b3ea366d7a932563
stopmin <geemin0@naver.com>
2024-07-16 14:38:45 +0900

    feat: 반응형 헤더 구현 - useEffect, useState로 스크롤 추적 - position: fixed, transition 설정
    #40

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,84 @@
-import { Button, Link, Stack, Typography } from '@mui/material';
+'use client';
 
+import React, { useState, useEffect } from 'react';
+import { Button, Link, Stack, Typography, Box } from '@mui/material';
 import { mainCategory } from '@/constants/category';
 import color from '@/constants/color';
 
 const Header = () => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlHeader = () => {
+    if (window.scrollY > lastScrollY) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlHeader);
+
+    return () => {
+      window.removeEventListener('scroll', controlHeader);
+    };
+  }, [lastScrollY]);
+
+
   return (
-    <Stack
-      alignItems="center"
-      direction="row"
-      justifyContent="space-between"
-      maxHeight="100px"
-      maxWidth="100%"
-      p={3}
-      px={6}
-    >
-      <Stack direction="row">
-        <Typography variant="h1">경단</Typography>
-        <Typography color={color.gray_dark} mt={2} variant="subtitle1">
-          경제를 단순하게
-        </Typography>
-      </Stack>
-      <Stack alignItems="center" direction="row" mt={2} spacing={6}>
-        {mainCategory.map((item) => (
-          <Link
-            key={item.id}
-            color={color.gray_dark}
-            href={item.path}
-            sx={{
-              '&:hover': {
-                backgroundColor: 'transparent',
-              },
-            }}
-            underline="none"
-            variant="h4"
-          >
-            {item.label}
-          </Link>
-        ))}
-        <Button color="primary" sx={{ width: '100px' }} variant="outlined">
-          LOGIN
-        </Button>
-      </Stack>
-    </Stack>
+    <>
+      <Box
+        sx={{
+          position: 'fixed',
+          top: show ? '0' : '-100px',
+          width: '100%',
+          transition: 'top 0.3s',
+          backgroundColor: 'white',
+          zIndex: 1000,
+        }}
+      >
+        <Stack
+          alignItems="center"
+          direction="row"
+          justifyContent="space-between"
+          height="100px"
+          maxWidth="100%"
+          p={3}
+          px={6}
+        >
+          <Stack direction="row">
+            <Typography variant="h1">경단</Typography>
+            <Typography color={color.gray_dark} mt={2} variant="subtitle1">
+              경제를 단순하게
+            </Typography>
+          </Stack>
+          <Stack alignItems="center" direction="row" mt={2} spacing={6}>
+            {mainCategory.map((item) => (
+              <Link
+                key={item.id}
+                color={color.gray_dark}
+                href={item.path}
+                sx={{
+                  '&:hover': {
+                    backgroundColor: 'transparent',
+                  },
+                }}
+                underline="none"
+                variant="h4"
+              >
+                {item.label}
+              </Link>
+            ))}
+            <Button color="primary" sx={{ width: '100px' }} variant="outlined">
+              LOGIN
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+      <Box height="100px" />
+    </>
   );
 };
 
