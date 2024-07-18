c2d74cd4cdd11dcbe3d1cf4f11ce7ab7037b195a
yeonddori <lsymiso@naver.com>
2024-07-18 09:00:39 +0900

    fix: 보안 정책 추가
    #55

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
+      <head>
+        <meta content="upgrade-insecure-requests" httpEquiv="Content-Security-Policy" />
+      </head>
       <body>
         <ClientThemeProvider>
           <Box minHeight="100vh" width="100vw">
