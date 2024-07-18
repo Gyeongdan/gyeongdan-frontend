75409dfae07fe001b3da6e13088843213022844d
yeonddori <126975394+yeonddori@users.noreply.github.com>
2024-07-19 06:15:17 +0900

    fix: 로그인 url 수정 (#74)
    #71

--- src/app/api/login.ts
+++ src/app/api/login.ts
@@ -2,4 +2,4 @@ import { springApiRequest } from './api';
 
 export const getKakaoLogin = () => springApiRequest('GET', '/login');
 
-export const postKakaoLogin = (code: string | null) => springApiRequest('POST', `/login/ok/${code}`);
+export const postKakaoLogin = (code: string | null) => springApiRequest('POST', `/login/ok?code=${code}`);
