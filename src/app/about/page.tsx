348361aec80d3825d3ebdd093ab4fcf9180a8809
stopmin <geemin0@naver.com>
2024-07-18 22:19:20 +0900

    feat: 기능 로고 추가 #61

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { Box } from '@mui/material';
+import GradientBox from '@/components/GradientBox';
+
+const Page = () => {
+  return (
+    <Box>
+      <GradientBox sx={{ height: '100vh' }} />
+    </Box>
+  );
+};
+
+export default Page;
