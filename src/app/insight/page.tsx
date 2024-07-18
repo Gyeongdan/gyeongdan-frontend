22a053d9a7711db8798681db8d1ee8eea02c5de7
stopmin <geemin0@naver.com>
2024-07-19 06:48:38 +0900

    feat: 동적으로 랜더링 #48

--- src/app/insight/page.tsx
+++ src/app/insight/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { Box, Stack, Typography, Grid } from '@mui/material';
-
-import ChartRenderer from '@/components/ChartRenderer';
+import dynamic from 'next/dynamic';
 import CommentCard from '@/components/CommentCard';
 import GradientBox from '@/components/GradientBox';
 import NewsCardVertical from '@/components/NewsCardVertical';
 import color from '@/constants/color';
 import insightData from '@/mocks/insight';
 import insightsDataList from '@/mocks/insights';
+
+const ChartRenderer = dynamic(() => import('@/components/ChartRenderer'), {
+  ssr: false,
+});
 
 const Page = () => {
   return (
