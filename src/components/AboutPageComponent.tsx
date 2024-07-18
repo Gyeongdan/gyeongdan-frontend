a9b9f40278648a9594cd9a17506fb9e8c7e3fff3
yeonddori <lsymiso@naver.com>
2024-07-19 08:43:03 +0900

    feat: 소개 페이지 구성
    #61

--- src/components/AboutPageComponent.tsx
+++ src/components/AboutPageComponent.tsx
@@ -1,30 +1,51 @@
+import Image from 'next/image';
 import React, { ReactNode } from 'react';
 
-import { Box, SxProps } from '@mui/material';
+import { Box, Stack, SxProps, Typography } from '@mui/material';
 
 interface AboutPageComponentProps {
   title?: string;
   description?: string;
+  category?: string;
   sx?: SxProps;
   style?: React.CSSProperties;
   children?: ReactNode;
+  imgageUrl?: string;
+  isright?: boolean;
 }
 
-  const AboutPageComponent = ({ title, description, sx, style, children }: AboutPageComponentProps) => {
-    return (
-      <Box
-        className="section"
-        sx={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          textAlign: 'center',
-          ...sx,
-          ...style,
+const AboutPageComponent = ({
+  title,
+  description,
+  category,
+  sx,
+  style,
+  children,
+  imgageUrl,
+  isright = false,
+}: AboutPageComponentProps) => {
+  return (
+    <Box
+      className="section"
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        whiteSpace: 'pre-line',
+        ...sx,
+        ...style,
       }}
     >
+      <Stack alignItems="center" direction="row" justifyContent={isright && 'end'} spacing={2}>
+        {imgageUrl && !isright && <Image alt="newsletter" height={100} src={imgageUrl} width={100} />}
+        <Typography color="primary" px={3} variant="h1">
+          {category}
+        </Typography>
+        {imgageUrl && isright && <Image alt="newsletter" height={100} src={imgageUrl} width={100} />}
+      </Stack>
       <div>
         {title && <h1>{title}</h1>}
         {description}
--- src/components/FullpageComponent.tsx
+++ src/components/FullpageComponent.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
 import ReactFullpage from '@fullpage/react-fullpage';
+import { Stack } from '@mui/material';
 
 import MainPage from '@/app/about/mainpage';
 import AboutPageComponent from '@/components/AboutPageComponent';
+
+import GradientBox from './GradientBox';
 
 const FullpageComponent: React.FC = () => (
   <ReactFullpage
@@ -13,13 +17,35 @@ const FullpageComponent: React.FC = () => (
         <div id="fullpage-wrapper" style={{ height: '100vh' }}>
           <MainPage />
           <AboutPageComponent
-            description="This is the about page"
-            style={{ backgroundColor: 'linear-gradient(to bottom, rgba(29, 112, 241, 0), rgba(26, 197, 251, 1))' }}
-            title="1번쨰"
+            category="홈 · 뉴스레터"
+            description={`경단을 통해 어려운 경제 기사를 쉽게 읽어보세요. AI로 분석하고 변환하여, 간편한 인터페이스로 제공합니다.\n 어려운 용어도 간단하게 설명해 드립니다.`}
+            imgageUrl="/images/newsletter.png"
+            sx={{ alignItems: 'flex-start', pl: '200px', pb: '80px', textAlign: 'left' }}
+            title={`내 경제 공부,\n 기사부터 인사이트까지\n 쉽고 똑똑하게`}
           />
-          <AboutPageComponent description="This is the about page" title="홈 ·뉴스레터" />
-          <AboutPageComponent description="This is the about page" title="기능 2개 설명" />
-          <AboutPageComponent description="This is the about page" title="우리가 주고 싶은 관점" />
+          <AboutPageComponent
+            isright
+            category="인사이트"
+            description={`공공데이터를 이용하여 경제 데이터를 분석하고, 시각화 자료와 함께 인사이트를 제공합니다.\n 어려운 데이터도 쉽게 이해할 수 있습니다.`}
+            imgageUrl="/images/insight.png"
+            sx={{ alignItems: 'flex-end', pr: '200px', pb: '80px', textAlign: 'right' }}
+            title="오늘의 인사이트 톺아보기"
+          />
+          <AboutPageComponent
+            category="챗봇"
+            description={`AI 산지니를 통해 경제 용어를 분석하고 변환하여, 대화형 인터페이스로 제공합니다.\n 어려운 용어도 간단하게 설명해 드립니다.`}
+            imgageUrl="/images/chatbot.png"
+            sx={{ alignItems: 'flex-start', pl: '200px', pb: '80px', textAlign: 'left' }}
+            title={`경제 단어부터 뉴스레터까지\n 쉽고 간편하게`}
+          />
+          <AboutPageComponent
+            isright
+            category="유형 검사"
+            description={`본인의 경제 관심사를 파악하고, 맞춤 기사를 추천받을 수 있습니다.\n AI가 분석하여, 당신에게 딱 맞는 기사를 추천합니다.`}
+            imgageUrl="/images/test.png"
+            sx={{ alignItems: 'flex-end', pr: '200px', pb: '80px', textAlign: 'right' }}
+            title={`유형검사하고\n 맞춤 기사 추천 받기`}
+          />
         </div>
       );
     }}
