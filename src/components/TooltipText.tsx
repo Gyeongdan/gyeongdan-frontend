b936d2bd96edaf112cfc6412f5b78fb73271296d
yeonddori <lsymiso@naver.com>
2024-07-20 00:25:28 +0900

    feat: TooltipText 중복 단어는 가장 처음 나오는 단어만 표시되도록 구현
    #68

--- src/components/TooltipText.tsx
+++ src/components/TooltipText.tsx
@@ -8,19 +8,27 @@ interface TooltipTextProps {
 const TooltipText = ({ content, phrasesContent }: TooltipTextProps) => {
   const regex = new RegExp(`(${Object.keys(phrasesContent).join('|')})`, 'g');
   const parts = content.split(regex);
+  const seen = new Set<string>();
 
   return (
     <>
       {parts.map((part) => {
-        const definition = phrasesContent[part.trim()];
-        return definition ? (
-          <Tooltip key={part} arrow sx={{ textDecoration: 'underline', cursor: 'pointer' }} title={definition}>
+        const cleanPart = part.trim();
+        const definition = phrasesContent[cleanPart];
+        const isTooltipNeeded = definition && !seen.has(cleanPart);
+
+        if (isTooltipNeeded) {
+          seen.add(cleanPart);
+        }
+
+        return isTooltipNeeded ? (
+          <Tooltip key={cleanPart} arrow sx={{ textDecoration: 'underline', cursor: 'pointer' }} title={definition}>
             <Typography component="span" fontSize="16px" variant="body2">
               {part}
             </Typography>
           </Tooltip>
         ) : (
-          <Typography key={part} component="span" fontSize="16px" variant="body2">
+          <Typography key={cleanPart} component="span" fontSize="16px" variant="body2">
             {part}
           </Typography>
         );
