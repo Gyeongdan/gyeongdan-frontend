927f25b733d6837b7733c26eb16ddc6103e58c17
stopmin <geemin0@naver.com>
2024-07-21 04:20:00 +0900

    feat: 이미지 허용 도메인 추가 #98

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'img.hankyung.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'pimg.mk.co.kr',
+      },
     ],
   },
 };
