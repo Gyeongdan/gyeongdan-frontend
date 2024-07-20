7d464672fdd7d50ae4e7882895e221ae45134b39
yeonddori <lsymiso@naver.com>
2024-07-20 20:27:40 +0900

    feat: 지난 인사이트 클릭 시 인사이트 내용이 바뀌도록 구현
    #87

--- src/app/insight/page.tsx
+++ src/app/insight/page.tsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import React, { useEffect, useState } from 'react';
 
 import { Box, Stack, Typography, Grid } from '@mui/material';
+import { Data } from 'plotly.js';
 
 import CommentCard from '@/components/CommentCard';
 import GradientBox from '@/components/GradientBox';
@@ -12,11 +13,24 @@ import color from '@/constants/color';
 import insightData from '@/mocks/insight';
 import insightsDataList from '@/mocks/insights';
 
+interface Insight {
+  id: number;
+  comment: string;
+  title: string;
+  content: string;
+  chart: { data: Data[]; layout: { title: string; xaxis: { title: string }; yaxis: { title: string } } };
+}
+
 // ChartRenderer를 동적으로 로드
 const ChartRenderer = dynamic(() => import('@/components/ChartRenderer'), { ssr: false });
 
 const Page = () => {
   const [isClient, setIsClient] = useState(false);
+  const [selectedInsight, setSelectedInsight] = useState(insightData);
+
+  const handleInsightClick = (insight: Insight) => {
+    setSelectedInsight(insight);
+  };
 
   useEffect(() => {
     setIsClient(true);
@@ -43,20 +57,20 @@ const Page = () => {
               justifyContent: 'space-between',
             }}
           >
-            <Box sx={{ flex: 1, mr: { md: '1rem', xs: 0 }, mb: { xs: '1.5rem', md: 0 } }}>
+            <Box maxWidth={700} sx={{ flex: 1, mr: { md: '1rem', xs: 0 }, mb: { xs: '1.5rem', md: 0 } }} width={700}>
               <Box mb="1.5rem">
-                <CommentCard isCharacter isStroke content={insightData.comment} />
+                <CommentCard isCharacter isStroke content={selectedInsight.comment} />
               </Box>
               <Box mb="1rem" width="650px">
                 <CommentCard
                   content={
                     <Box>
                       <Typography color={color.gradient_blue_dark} variant="h4">
-                        {insightData.title}
+                        {selectedInsight.title}
                       </Typography>
                       {'\n'}
                       <Typography color={color.gray_dark} variant="body1">
-                        {insightData.content}
+                        {selectedInsight.content}
                       </Typography>
                     </Box>
                   }
@@ -64,7 +78,12 @@ const Page = () => {
               </Box>
             </Box>
             <Box sx={{ flex: 1 }}>
-              <ChartRenderer data={insightData.chart.data} height={320} layout={insightData.chart.layout} width={480} />
+              <ChartRenderer
+                data={selectedInsight.chart.data}
+                height={320}
+                layout={selectedInsight.chart.layout}
+                width={480}
+              />
               <Box mt="1rem" textAlign="center" width="480px">
                 <CommentCard isFilled content="나만의 인사이트 만들어보기(공사중 🚧)" />
               </Box>
@@ -75,14 +94,14 @@ const Page = () => {
               지난 인사이트
             </Typography>
             <Grid container spacing={2} sx={{ marginTop: '1rem', maxWidth: '1500px' }}>
-              {insightsDataList.slice(0, 3).map((_insightData) => (
-                <Grid key={_insightData.id} item md={4} xs={12}>
-                  <Box style={{ borderRadius: 24 }}>
-                    <NewsCardVertical
-                      date={_insightData.date}
-                      description={_insightData.content}
-                      title={_insightData.title}
-                    />
+              {insightsDataList.slice(0, 3).map((insight) => (
+                <Grid key={insight.id} item md={4} xs={12}>
+                  <Box
+                    style={{ borderRadius: 24 }}
+                    sx={{ cursor: 'pointer' }}
+                    onClick={() => handleInsightClick(insight)}
+                  >
+                    <NewsCardVertical date={insight.date} description={insight.content} title={insight.title} />
                   </Box>
                 </Grid>
               ))}
