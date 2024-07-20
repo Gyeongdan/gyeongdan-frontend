54230c2bae965295a59a18e73d01c87a1380c727
yeonddori <lsymiso@naver.com>
2024-07-21 03:33:36 +0900

    refactor: postUserType token 타입에 null 제거
    #97

--- src/app/api/user.ts
+++ src/app/api/user.ts
@@ -2,5 +2,5 @@ import { springApiRequest } from './api';
 
 export const getUser = (token: string) => springApiRequest('GET', '/api/user/profile', token);
 
-export const postUserType = (data: { answers: number[] }, token: string | null) =>
+export const postUserType = (data: { answers: number[] }, token: string) =>
   springApiRequest('POST', '/api/usertype/save', token, data);
